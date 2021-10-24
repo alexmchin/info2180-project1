98a958cc7506f2e93310b367496bc385f02c6cba
alexmchin <alexmchin12@gmail.com>
2021-10-24 15:18:36 -0500

    Made format and design changes to website

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 /* Add your JavaScript to this file */
+
+var email = document.getElementById("email");
+var button = document.querySelector("button");
+
+button.addEventListener("click", clicked =>{
+  email=> email.innerHTML = "Thank You";
+});
